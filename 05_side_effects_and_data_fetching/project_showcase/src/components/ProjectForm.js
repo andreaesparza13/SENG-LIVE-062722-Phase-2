9dfae3b30e43281e0481a49983524b3eba224713
Andrea <acesparza05@gmail.com>
2022-07-26 12:57:25 -0500

    lecture 5 code along

--- 05_side_effects_and_data_fetching/project_showcase/src/components/ProjectForm.js
+++ 05_side_effects_and_data_fetching/project_showcase/src/components/ProjectForm.js
@@ -23,9 +23,17 @@ const ProjectForm = ({ onAddProject }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
-    onAddProject(formData);
-    setFormData(initialFormState);
+    fetch("http://localhost:4000/projects", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(res => res.json())
+      .then(newProject => {onAddProject(newProject)})
+    // onAddProject(formData); <---- OPTIMISTIC rendering
+    setFormData(initialFormState)
   }
 
   return (
